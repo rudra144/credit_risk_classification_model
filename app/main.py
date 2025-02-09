e71acd20a17dd5b1b676bf67e4373ded53b1269a
rudra144 <147151654+rudra144@users.noreply.github.com>
2025-02-09 11:20:45 +0100

    name_update

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ import streamlit as st
 from prediction_helper import predict  # Ensure this is correctly linked to your prediction_helper.py
 
 # Set the page configuration and title
-st.set_page_config(page_title="Lauki Finance: Credit Risk Modelling", page_icon="📊")
-st.title("Lauki Finance: Credit Risk Modelling")
+st.set_page_config(page_title="Credit Risk Modelling", page_icon="📊")
+st.title("Credit Risk Modelling")
 
 # Create rows of three columns each
 row1 = st.columns(3)
@@ -62,5 +62,3 @@ if st.button('Calculate Risk'):
     st.write(f"Credit Score: {credit_score}")
     st.write(f"Rating: {rating}")
 
-# Footer
-# st.markdown('_Project From Codebasics ML Course_')
